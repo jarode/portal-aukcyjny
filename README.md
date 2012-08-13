6a12d8a4a1a4475b33ee177014b1fe3ad0b0fa2d
jarode <jarode2@o2.pl>
2012-08-13 18:32:36 +0100

    .

---
+++ README.md
@@ -0,0 +1,11 @@
+Ruby on Rails 3.2
+
+Nazwa Projektu: Portal Aukcyjny
+
+Autor : Jarek Karwat jarode2@gmail.com
+
+Opis: ...
+
+data rozpoczęcia: 13 sierpień 2012 
+
+github.com: git@github.com:jarode/portal-aukcyjny.git
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portalaukcyjny::Application.config.secret_token = '7ac69cb5f04f0af58aeecf07e240893900c6eda35499e9ae3ac11df892a5ab28f2f53b887e03b92ae734e552f27041e81c30712283c8b518576b228b8a3085fe'
